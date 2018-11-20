b0bf2496c6972cd7201e128694d58d53f04130e4
Nedeljko Mijajlovic <stoopro2@gmail.com>
2018-11-20 11:11:59 +0100

    Initial Commit

---
+++ MiraclePills/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiraclePills
+//
+//  Created by Nedeljko Mijajlovic on 20.11.2018.
+//  Copyright © 2018 Nedeljko Mijajlovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
