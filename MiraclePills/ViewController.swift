8f45bb71d79da7d773ee80909c25d17d634b0253
Nedeljko Mijajlovic <stoopro2@gmail.com>
2018-11-21 08:34:19 +0100

    layout and buttons for aplication

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func stateButtonPressed(_ sender: Any) {
+    }
 
 }
 
